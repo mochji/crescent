b31e7d2ec20c09fe36479aebc8a322be12bc907d
mochji <owo.mochji@gmail.com>
2024-01-05 02:10:07 -0500

    object structs

---
+++ src/core/object.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdint.h>
+
+typedef int64_t crescent_Integer;
+typedef double  crescent_Float;
+
+enum crescent_Type {
+	CRESCENT_TINTEGER,
+	CRESCENT_TFLOAT
+};
+
+union crescent_Value {
+	crescent_Integer i;
+	crescent_Float   f;
+};
+
+struct crescent_Object {
+	enum  crescent_Type  type;
+	union crescent_Value value;
+};
+
+typedef enum   crescent_Type   crescent_Type;
+typedef union  crescent_Value  crescent_Value;
+typedef struct crescent_Object crescent_Object;
