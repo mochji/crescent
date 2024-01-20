393032c5e380794a5c2e5485ebbc80d276b1b03b
mochji <owo.mochji@gmail.com>
2024-01-19 22:54:36 -0500

    core/state stuff

--- src/core/state.c
+++ src/core/state.c
@@ -1,36 +1,66 @@
-/*
- * https://github.com/mochji/crescent
- * core/state.c
- *
- * idrk what this is
- * Copyright (C) 2024 mochji
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 #include <stdlib.h>
 #include <stddef.h>
 
-#include "../conf.h"
+#include "conf.h"
 
-#include "object.h"
+#include "core/object.h"
 
-struct crescent_State {
+struct
+crescent_State {
 	struct crescent_Stack {
 		size_t           size;
-		size_t           base;
-		size_t           pointer;
+		size_t           top;
 		crescent_Object* data;
 	} stack;
 };
+
+typedef struct crescent_State crescent_State;
+
+int
+crescent_reallocStack(crescent_State* state, size_t newTop) {
+	if (state->stack.size - newTop >= CRESCENT_STACK_MINFREE && state->stack.size <= CRESCENT_STACK_MAXFREE) {
+		return 0;
+	}
+
+	crescent_Object* newData = realloc(state->stack.data, state->stack.size);
+
+	if (newData == NULL) {
+		free(state->stack.data);
+		free(state);
+
+		return 1;
+	}
+
+	state->stack.size = newTop + CRESCENT_STACK_INITSIZE;
+	state->stack.top  = newTop;
+	state->stack.data = newData;
+
+	return 0;
+}
+
+crescent_State*
+crescent_openState() {
+	crescent_State* state = malloc(sizeof(crescent_State));
+
+	if (state == NULL) {
+		return NULL;
+	}
+
+	state->stack.size = CRESCENT_STACK_INITSIZE;
+	state->stack.top  = 0;
+	state->stack.data = calloc(state->stack.size, sizeof(crescent_Object));
+
+	if (state->stack.data == NULL) {
+		free(state);
+
+		return NULL;
+	}
+
+	return state;
+}
+
+void
+crescent_closeState(crescent_State* state) {
+	free(state->stack.data);
+	free(state);
+}
--- src/core/state.h
+++ src/core/state.h
@@ -1,41 +1,31 @@
-/*
- * https://github.com/mochji/crescent
- * core/state.h
- *
- * idrk what this is
- * Copyright (C) 2024 mochji
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 #ifndef CORE_STATE_H
 #define CORE_STATE_H
 
 #include <stdlib.h>
 #include <stddef.h>
 
-#include "../conf.h"
+#include "conf.h"
 
-#include "object.h"
+#include "core/object.h"
 
-struct crescent_State {
+struct
+crescent_State {
 	struct crescent_Stack {
 		size_t           size;
-		size_t           base;
-		size_t           pointer;
+		size_t           top;
 		crescent_Object* data;
 	} stack;
 };
+
+typedef struct crescent_State crescent_State;
+
+extern int*
+crescent_reallocStack(crescent_State* state, size_t newTop);
+
+extern crescent_State*
+crescent_openState();
+
+extern void
+crescent_closeState(crescent_State* state);
 
 #endif
