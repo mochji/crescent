787434da62ad58b6c34e2eef403aa83f679b97ab
mochji <owo.mochji@gmail.com>
2024-01-20 23:12:34 -0500

    better state structure and moved functions to api

--- src/core/state.c
+++ src/core/state.c
@@ -5,62 +5,40 @@
 
 #include "core/object.h"
 
+enum
+crescent_CallType {
+	CALLTYPE_C,
+	CALLTYPE_CRESCENT
+};
+
 struct
-crescent_State {
-	struct crescent_Stack {
+crescent_Frame {
+	union CallInfo {
+		struct crescent {
+			size_t savedPC;
+		} crescent;
+		struct c {
+		} c;
+	} callInfo;
+	struct Stack {
 		size_t           size;
 		size_t           top;
 		crescent_Object* data;
 	} stack;
+	int callType;
 };
 
+struct
+crescent_Stack {
+	size_t                 frameCount;
+	struct crescent_Frame* frames;
+	struct crescent_Frame* topFrame;
+};
+
+struct
+crescent_State {
+	struct crescent_Stack stack;
+};
+
+typedef struct crescent_Frame crescent_Frame;
 typedef struct crescent_State crescent_State;
-
-int
-crescent_reallocStack(crescent_State* state, size_t newTop) {
-	if (state->stack.size - newTop >= CRESCENT_STACK_MINFREE && state->stack.size <= CRESCENT_STACK_MAXFREE) {
-		return 0;
-	}
-
-	crescent_Object* newData = realloc(state->stack.data, state->stack.size);
-
-	if (newData == NULL) {
-		free(state->stack.data);
-		free(state);
-
-		return 1;
-	}
-
-	state->stack.size = newTop + CRESCENT_STACK_INITSIZE;
-	state->stack.top  = newTop;
-	state->stack.data = newData;
-
-	return 0;
-}
-
-crescent_State*
-crescent_openState() {
-	crescent_State* state = malloc(sizeof(crescent_State));
-
-	if (state == NULL) {
-		return NULL;
-	}
-
-	state->stack.size = CRESCENT_STACK_INITSIZE;
-	state->stack.top  = 0;
-	state->stack.data = calloc(state->stack.size, sizeof(crescent_Object));
-
-	if (state->stack.data == NULL) {
-		free(state);
-
-		return NULL;
-	}
-
-	return state;
-}
-
-void
-crescent_closeState(crescent_State* state) {
-	free(state->stack.data);
-	free(state);
-}
--- src/core/state.h
+++ src/core/state.h
@@ -8,24 +8,42 @@
 
 #include "core/object.h"
 
+enum
+crescent_CallType {
+	CALLTYPE_C,
+	CALLTYPE_CRESCENT
+};
+
 struct
-crescent_State {
-	struct crescent_Stack {
+crescent_Frame {
+	union CallInfo {
+		struct crescent {
+			size_t savedPC;
+		} crescent;
+		struct c {
+		} c;
+	} callInfo;
+	struct Stack {
 		size_t           size;
 		size_t           top;
 		crescent_Object* data;
 	} stack;
+	int callType;
 };
 
+struct
+crescent_Stack {
+	size_t                  frameCount;
+	struct crescent_Frame** frames;
+	struct crescent_Frame*  topFrame;
+};
+
+struct
+crescent_State {
+	struct crescent_Stack stack;
+};
+
+typedef struct crescent_Frame crescent_Frame;
 typedef struct crescent_State crescent_State;
-
-extern int*
-crescent_reallocStack(crescent_State* state, size_t newTop);
-
-extern crescent_State*
-crescent_openState();
-
-extern void
-crescent_closeState(crescent_State* state);
 
 #endif
