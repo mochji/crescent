a05cdf1665acbafb47a3961da1a8ca9aafa3d42a
mochji <owo.mochji@gmail.com>
2024-02-06 09:34:19 -0500

    Reapply "asdasdasd"
    This reverts commit ee446f08563010ffe8792cead00b1047be91e980.

--- src/core/call.c
+++ src/core/call.c
@@ -32,14 +32,23 @@
 
 int
 crescentC_setError(crescent_State* state, crescent_Status status) {
-	size_t index = state->stack.topFrame->base + state->stack.topFrame->top - 1;
+	size_t index = state->stack.topFrame->base + state->stack.topFrame->top;
 
 	switch (status) {
 		case CRESCENT_STATUS_ERRMEM:
+			state->stack.topFrame->top      += 1;
 			state->stack.data[index].type    = CRESCENT_TYPE_STRING;
 			state->stack.data[index].value.s = state->gState->memoryErrorMsg;
 
 			break;
+		case CRESCENT_STATUS_ERRC:
+			state->stack.topFrame->top      += 1;
+			state->stack.data[index].type    = CRESCENT_TYPE_STRING;
+			state->stack.data[index].value.s = malloc(sizeof(crescent_String));
+
+			if (state->stack.data[index].value.s == NULL) {
+				crescentC_throw(state, CRESCENT_STATUS_ERRMEM);
+			}
 		case CRESCENT_STATUS_ERROR:
 			break;
 		default:
