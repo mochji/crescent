bf1e50c0d266df48241b80e6a1f926ffa2bafb9f
mochji <owo.mochji@gmail.com>
2024-04-05 19:10:26 -0400

    fix typo and set NEW string's reference counter to 1 in crescentS_clone

--- src/types/string.c
+++ src/types/string.c
@@ -81,7 +81,7 @@ crescentS_clone(crescent_String* string) {
 	cloned->size       = string->size;
 	cloned->length     = string->length;
 	cloned->data       = malloc(string->size);
-	string->references = 1;
+	cloned->references = 1;
 
 	if (cloned->data == NULL) {
 		free(cloned);
