f0bc8f6216d39cca8159d29108855acee3756b8f
mochji <owo.mochji@gmail.com>
2024-04-14 14:22:51 -0400

    try to convert boolean to string

--- src/core/object.c
+++ src/core/object.c
@@ -248,6 +248,10 @@ crescentO_toString(crescent_Object* object, int* isString) {
 		*isString = 0;
 	}
 
+	if (object->type == CRESCENT_TYPE_BOOLEAN) {
+		return object->value.b ? "true" : "false";
+	}
+
 	return NULL;
 }
 
