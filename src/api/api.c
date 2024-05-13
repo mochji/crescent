0dc88549aa9369b447c30e5df2ab34f1f93a924f
mochji <owo.mochji@gmail.com>
2024-05-13 05:47:40 -0400

    move call functions to vm/vm

--- src/api/api.c
+++ src/api/api.c
@@ -33,6 +33,7 @@
 #include "core/object.h"
 #include "core/state.h"
 #include "core/call.h"
+#include "vm/vm.h"
 
 /* TODO: throw errors on... stuff that cause errors idrk */
 
@@ -461,41 +462,31 @@ crescent_remove(crescent_State* state, size_t index) {
 }
 
 int
-crescent_callC(crescent_State* state, crescent_CFunction* function, size_t argCount) {
-	return crescentC_callC(state, function, argCount, INT_MAX);
-}
-
-int
-crescent_pCallC(crescent_State* state, crescent_CFunction* function, size_t argCount, int* status) {
-	return crescentC_pCallC(state, function, argCount, INT_MAX, status);
-}
-
-int
 crescent_call(crescent_State* state, size_t index, size_t argCount) {
-	crescent_CFunction* function = state->stack.data[api_absindex(state, index)].value.c;
+	crescent_Object* object = &state->stack.data[api_absindex(state, index)];
 
-	return crescentC_callC(state, function, argCount, INT_MAX);
+	return crescentV_call(state, object, argCount, INT_MAX);
 }
 
 int
 crescent_pCall(crescent_State* state, size_t index, size_t argCount, int* status) {
-	crescent_CFunction* function = state->stack.data[api_absindex(state, index)].value.c;
+	crescent_Object* object = &state->stack.data[api_absindex(state, index)];
 
-	return crescentC_pCallC(state, function, argCount, INT_MAX, status);
+	return crescentV_pCall(state, object, argCount, INT_MAX, status);
 }
 
 int
 crescent_callK(crescent_State* state, size_t index, size_t argCount, int maxResults) {
-	crescent_CFunction* function = state->stack.data[api_absindex(state, index)].value.c;
+	crescent_Object* object = &state->stack.data[api_absindex(state, index)];
 
-	return crescentC_callC(state, function, argCount, maxResults);
+	return crescentV_call(state, object, argCount, maxResults);
 }
 
 int
 crescent_pCallK(crescent_State* state, size_t index, size_t argCount, int maxResults, int* status) {
-	crescent_CFunction* function = state->stack.data[api_absindex(state, index)].value.c;
+	crescent_Object* object = &state->stack.data[api_absindex(state, index)];
 
-	return crescentC_pCallC(state, function, argCount, maxResults, status);
+	return crescentV_pCall(state, object, argCount, maxResults, status);
 }
 
 int
--- src/vm/vm.c
+++ src/vm/vm.c
@@ -19,9 +19,14 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <stddef.h>
+#include <setjmp.h>
+
 #include "conf.h"
 
 #include "core/object.h"
+#include "core/state.h"
+#include "core/call.h"
 
 /*
  * This function is a more loose comparison compared to crescentO_compare, as
@@ -42,7 +47,103 @@ crescentV_compare(crescent_Object* a, crescent_Object* b) {
 		if (aType == CRESCENT_TYPE_FLOAT && bType == CRESCENT_TYPE_INTEGER) {
 			return a->value.f == (crescent_Float)b->value.i;
 		}
+
+		return 0;
 	}
 
 	return crescentO_compare(a, b);
+}
+
+/* TODO: this is a temporary function until string format */
+
+static void
+crescentV_callError(crescent_State* state, int type) {
+	switch (type) {
+		case CRESCENT_TYPE_NIL:
+			crescentC_setError(state, "attempt to call a nil value");
+
+			break;
+		case CRESCENT_TYPE_BOOLEAN:
+			crescentC_setError(state, "attempt to call a boolean value");
+
+			break;
+		case CRESCENT_TYPE_INTEGER:
+			crescentC_setError(state, "attempt to call a number value");
+
+			break;
+		case CRESCENT_TYPE_FLOAT:
+			crescentC_setError(state, "attempt to call a number value");
+
+			break;
+		case CRESCENT_TYPE_STRING:
+			crescentC_setError(state, "attempt to call a string value");
+
+			break;
+		case CRESCENT_TYPE_ARRAY:
+			crescentC_setError(state, "attempt to call a array value");
+
+			break;
+	}
+}
+
+int
+crescentV_call(crescent_State* state, crescent_Object* object, int argCount, int maxResults) {
+	if (object->type != CRESCENT_TYPE_CFUNCTION) {
+		crescentV_callError(state, object->type);
+	}
+
+	crescent_Frame newTopFrame;
+	int            results;
+
+	crescentC_startCall(state, argCount, &newTopFrame);
+
+	results = object->value.c(state);
+
+	if (results < 0) {
+		results = 0;
+	} else if (results > maxResults) {
+		results = maxResults;
+	}
+
+	crescentC_endCall(state, results);
+	crescentC_resizeStack(state, state->stack.topFrame->top, 1);
+
+	return results;
+}
+
+int
+crescentV_pCall(crescent_State* state, crescent_Object* object, int argCount, int maxResults, int* status) {
+	if (object->type != CRESCENT_TYPE_CFUNCTION) {
+		crescentV_callError(state, object->type);
+	}
+
+	crescent_ErrorJump* oldErrorJump  = state->errorJump;
+	crescent_ErrorJump  newErrorJump  = {.status = CRESCENT_STATUS_OK};
+	size_t              oldFrameCount = state->stack.frames;
+	int                 results;
+
+	state->errorJump = &newErrorJump;
+
+	if (setjmp(newErrorJump.buffer) == 0) {
+		results = crescentV_call(state, object, argCount, maxResults);
+	} else {
+		for (size_t a = oldFrameCount; a < state->stack.frames; a++) {
+			crescentC_endCall(state, 0);
+		}
+
+		if (crescentC_resizeStack(state, state->stack.topFrame->top, 0)) {
+			crescentC_setError(state, NULL);
+			state->error = state->memoryError;
+		}
+
+		results = 0;
+	}
+
+	state->errorJump = oldErrorJump;
+
+	if (status != NULL) {
+		*status = newErrorJump.status;
+	}
+
+	return results;
 }
