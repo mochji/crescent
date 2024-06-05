ebaac44eed53958a3816fd30a125b5f85d4b14c3
mochji <owo.mochji@gmail.com>
2024-06-05 10:33:24 -0400

    add max for crescent_Integer, size_t and object size

--- src/limit.h
+++ src/limit.h
@@ -11,6 +11,18 @@
 #ifndef CRESCENT_LIMIT_H
 #define CRESCENT_LIMIT_H
 
+#include <stddef.h>
+
+#include "conf.h"
+
+#define CRESCENT_MAX_INTEGER (~((crescent_Integer)0))
+#define CRESCENT_MAX_SIZET   (~((size_t)0))
+
+#define CRESCENT_MAX_SIZE                         \
+	(CRESCENT_MAX_SIZET > CRESCENT_MAX_INTEGERT ? \
+		CRESCENT_MAX_INTEGER :                    \
+		CRESCENT_MAX_SIZET                        \
+	)
 
 #define CRESCENT_MAX_CALLS  4096
 #define CRESCENT_MAX_CCALLS 200
