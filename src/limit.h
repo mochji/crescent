51cb0989d66e23c21c32de57c2651225a8481c1a
mochji <owo.mochji@gmail.com>
2024-06-24 21:31:26 -0400

    add stack limits

--- src/limit.h
+++ src/limit.h
@@ -31,7 +31,15 @@
 		CRESCENT_MAX_SIZET                       \
 	)
 
+/* TODO: actually use MAX_STACK, MIN_TOP and MAX_TOP */
+
+/* global to entire stack */
 #define CRESCENT_MIN_STACK 64
+#define CRESCENT_MAX_STACK CRESCENT_MAX_SIZET
+
+/* local to a stack frame */
+#define CRESCENT_MIN_TOP 8
+#define CRESCENT_MAX_TOP (INT_MAX - 1)
 
 #define CRESCENT_MAX_CALLS  4096
 #define CRESCENT_MAX_CCALLS 200
