57f3d2ed0b6c5f685c48cef212c0af40d41399cf
mochji <owo.mochji@gmail.com>
2024-09-08 21:47:22 -0400

    rewrite configuration files (conf.h and limit.h)

--- src/conf.h
+++ src/conf.h
@@ -10,6 +10,9 @@
  * Public build configuration for Crescent
  *
  * Search '@' for all configurable definitions.
+ *
+ * You should change the definitions here, rather than through -D, as other
+ * code connected to Crescent must (mostly) use the same configuration.
  */
 
 #ifndef CRESCENT_CONF_H
@@ -32,7 +35,6 @@
 
 #define CRESCENT_VERSION (CRESCENT_VERSION_MAJOR * 10 + CRESCENT_VERSION_MINOR)
 #define CRESCENT_RELEASE (CRESCENT_VERSION * 10 + CRESCENT_VERSION_PATCH)
-#define CRESCENT_AUTHORS "mochji"
 
 /*
  * ============================================================================
@@ -43,91 +45,69 @@
  * ============================================================================
  */
 
+#define CRESCENT_32INT (UINT_MAX >> 32)
+
+/*
+ * ============================================================================
+ * Type configuration
+ * ============================================================================
+ */
+
+#define CRESCENT_INTEGER_INT   0
+#define CRESCENT_INTEGER_LONG  1
+#define CRESCENT_INTEGER_LLONG 2
+
+#define CRESCENT_FLOAT_FLOAT   0
+#define CRESCENT_FLOAT_DOUBLE  0
+#define CRESCENT_FLOAT_LDOUBLE 0
+
 /*
  * @ CRESCENT_32BIT
  *
  * Change this definition to a non-zero value to restrict Crescent to 32-bit
- * number types. (default uses 64-bit types)
+ * number types, rather than the default 64-bit types.
  */
 
 #define CRESCENT_32BIT 0
 
 /*
- * ============================================================================
- * Crescent string configuration
+ * @ CRESCENT_INTEGER_TYPE
+ * @ CRESCENT_FLOAT_TYPE
  *
- * Definitions that control the behavior of Crescent strings, mainly memory
- * allocation.
- * ============================================================================
+ * Type of Crescent integers and floats.
  */
+
+#if CRESCENT_32BIT
+#	if CRESCENT_32INT
+#		define CRESCENT_INTEGER_TYPE CRESCENT_INTEGER_INT
+#	else
+#		define CRESCENT_INTEGER_TYPE CRESCENT_INTEGER_LONG
+#	endif
+#	define CRESCENT_FLOAT_TYPE       CRESCENT_FLOAT_FLOAT
+#else
+#	define CRESCENT_INTEGER_TYPE CRESCENT_INTEGER_LLONG
+#	define CRESCENT_FLOAT_TYPE   CRESCENT_FLOAT_DOUBLE
+#endif
 
 /*
  * @ CRESCENT_STRING_ALLOCSPACE
+ * @ CRESCENT_ARRAY_ALLOCSPACE
  *
- * How much is added to the length when allocating or reallocating to get the
- * total allocated size for the string.
+ * THESE DEFINITIONS WILL BE REMOVED !!
  */
 
 #define CRESCENT_STRING_ALLOCSPACE 32
-
-/*
- * ============================================================================
- * Crescent array configuration
- *
- * Definitions that control the behavior of Crescent arrays, mainly memory
- * allocation.
- * ============================================================================
- */
-
-/*
- * @ CRESCENT_ARRAY_ALLOCSPACE
- *
- * How much is added to the length when allocating or reallocating to get the
- * total allocated size for the array.
- */
-
 #define CRESCENT_ARRAY_ALLOCSPACE 32
 
 /*
  * ============================================================================
- * End of configurable definitions
+ * End of configurable options
  * ============================================================================
  */
 
 #ifdef __cplusplus
-#	error Crescent is not supported for C++. (not yet)
+#	error Crescent is not yet supported for C++.
 #endif
-
-/* cannot use sizeof within a preprocessor macro :( */
-
-#define CRESCENT_32INT ((UINT_MAX >> 30) >= 3)
-
-#if CRESCENT_32BIT
-#	if CRESCENT_32INT
-#		define CRESCENT_INTEGER     int
-#		define CRESCENT_INTEGER_FMT "%d"
-#		define CRESCENT_INTEGER_MAX INT_MAX
-#		define CRESCENT_INTEGER_MIN INT_MIN
-#	else
-#		define CRESCENT_INTEGER     long
-#		define CRESCENT_INTEGER_FMT "%ld"
-#		define CRESCENT_INTEGER_MAX LONG_MAX
-#		define CRESCENT_INTEGER_MIN LONG_MIN
-#	endif
-#	define CRESCENT_FLOAT     float
-#	define CRESCENT_FLOAT_FMT "%f"
-#else
-#	define CRESCENT_INTEGER     long long
-#	define CRESCENT_INTEGER_FMT "%lld"
-#	define CRESCENT_INTEGER_MAX LLONG_MAX
-#	define CRESCENT_INTEGER_MIN LLONG_MIN
-#	define CRESCENT_FLOAT       double
-#	define CRESCENT_FLOAT_FMT   "%lf"
-#endif
-
-#define CRESCENT_STATUS_OK    0
-#define CRESCENT_STATUS_ERROR 1
-#define CRESCENT_STATUS_NOMEM 2
 
 /*
  * The last 3 bits within a type tag encode properties about the type:
@@ -146,6 +126,38 @@
 #define CRESCENT_TYPE_STRING    2  /* 0b00000010 */
 #define CRESCENT_TYPE_ARRAY     10 /* 0b00001010 */
 #define CRESCENT_TYPE_CFUNCTION 4  /* 0b00000100 */
+
+#define CRESCENT_STATUS_OK    0
+#define CRESCENT_STATUS_ERROR 1
+#define CRESCENT_STATUS_NOMEM 2
+
+#if CRESCENT_INTEGER_TYPE == CRESCENT_INTEGER_INT
+#	define CRESCENT_INTEGER     int
+#	define CRESCENT_INTEGER_FMT "%d"
+#	define CRESCENT_INTEGER_MAX INT_MAX
+#	define CRESCENT_INTEGER_MIN INT_MIN
+#elif CRESCENT_INTEGER_TYPE == CRESCENT_INTEGER_LONG
+#	define CRESCENT_INTEGER     long
+#	define CRESCENT_INTEGER_FMT "%ld"
+#	define CRESCENT_INTEGER_MAX LONG_MAX
+#	define CRESCENT_INTEGER_MIN LONG_MIN
+#elif CRESCENT_INTEGER_TYPE == CRESCENT_INTEGER_LLONG
+#	define CRESCENT_INTEGER     long long
+#	define CRESCENT_INTEGER_FMT "%lld"
+#	define CRESCENT_INTEGER_MAX LLONG_MAX
+#	define CRESCENT_INTEGER_MIN LLONG_MIN
+#endif
+
+#if CRESCENT_FLOAT_TYPE == CRESCENT_FLOAT_FLOAT
+#	define CRESCENT_FLOAT     float
+#	define CRESCENT_FLOAT_FMT "%f"
+#elif CRESCENT_FLOAT_TYPE == CRESCENT_FLOAT_DOUBLE
+#	define CRESCENT_FLOAT     double
+#	define CRESCENT_FLOAT_FMT "%lf"
+#elif CRESCENT_FLOAT_TYPE == CRESCENT_FLOAT_LDOUBLE
+#	define CRESCENT_FLOAT     long double
+#	define CRESCENT_FLOAT_FMT "%llf"
+#endif
 
 struct crescent_State;
 
