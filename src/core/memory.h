7a52443eaa00ecd434761819a0c40a2c7a3b6efa
mochji <owo.mochji@gmail.com>
2025-12-20 22:56:33 -0600

    add macros for the aforementioned wrapper functions

--- src/core/memory.h
+++ src/core/memory.h
@@ -23,4 +23,15 @@ crsM_realloc_(crs_Thread* thread, void* block, size_t size, size_t oldSize);
 extern void
 crsM_free_(crs_Thread* thread, void* block, size_t size);
 
+#define obj_new(t, o)  (crsM_malloc_((t), sizeof(o)))
+#define obj_free(t, o) (crsM_malloc_((t), sizeof(*o)))
+
+#define vec_new(t, n, o)          (crsM_malloc_((t), (n) * sizeof(o)))
+#define vec_resize(t, v, n, p, o) (crsM_realloc_((t), (v), (n) * sizeof(o), (p) * sizeof(o)))
+#define vec_free(t, v, n, o)      (crsM_free_((t), (v), (n) * sizeof(o)))
+
+#define str_new(t, n)          (crsM_malloc_((t), (n) * sizeof(char)))
+#define str_resize(t, s, n, p) (crsM_realloc_((t), (s), (n) * sizeof(char), (p) * sizeof(char)))
+#define str_free(t, s, n)      (crsM_free_((t), (s), (n) * sizeof(char)))
+
 #endif
