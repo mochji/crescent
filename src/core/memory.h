0755d372413a887ef6514e813c2eaabb2528aa8f
mochji <owo.mochji@gmail.com>
2026-01-14 08:45:28 -0500

    remove obj_new and obj_free. rename vec_* and str_*

--- src/core/memory.h
+++ src/core/memory.h
@@ -23,15 +23,12 @@ crsM_realloc_(crs_Thread* thread, void* block, size_t size, size_t oldSize);
 extern void
 crsM_free_(crs_Thread* thread, void* block, size_t size);
 
-#define obj_new(t, o, f)  (crsM_malloc_((t), sizeof(o) + (f)))
-#define obj_free(t, o, f) (crsM_free_((t), (o), sizeof(*o) + (f)))
+#define mem_vnew(t, n, o)          (crsM_malloc_((t), (n) * sizeof(o)))
+#define mem_vresize(t, v, n, p, o) (crsM_realloc_((t), (v), (n) * sizeof(o), (p) * sizeof(o)))
+#define mem_vfree(t, v, n, o)      (crsM_free_((t), (v), (n) * sizeof(o)))
 
-#define vec_new(t, n, o)          (crsM_malloc_((t), (n) * sizeof(o)))
-#define vec_resize(t, v, n, p, o) (crsM_realloc_((t), (v), (n) * sizeof(o), (p) * sizeof(o)))
-#define vec_free(t, v, n, o)      (crsM_free_((t), (v), (n) * sizeof(o)))
-
-#define str_new(t, n)          (crsM_malloc_((t), (n) * sizeof(char)))
-#define str_resize(t, s, n, p) (crsM_realloc_((t), (s), (n) * sizeof(char), (p) * sizeof(char)))
-#define str_free(t, s, n)      (crsM_free_((t), (s), (n) * sizeof(char)))
+#define mem_snew(t, n)          (crsM_malloc_((t), (n) * sizeof(char)))
+#define mem_sresize(t, s, n, p) (crsM_realloc_((t), (s), (n) * sizeof(char), (p) * sizeof(char)))
+#define mem_sfree(t, s, n)      (crsM_free_((t), (s), (n) * sizeof(char)))
 
 #endif
