97da08664c47891004f67606df548cddca26362c
mochji <owo.mochji@gmail.com>
2026-01-29 05:24:16 -0500

    0_0

--- src/core/gc.c
+++ src/core/gc.c
@@ -331,8 +331,7 @@ incremental_step(crs_State* state) {
 	}
 
 	if (state->gc.phase == CRS_GCPHASE_RESTART) {
-		crs_mem pause = gc_getparam(state, PAUSE);
-		setPause(state, applyParam(state->gc.usage, pause));
+		setPause(state, applyParam(state->gc.usage, gc_getparam(state, PAUSE));
 
 		return 1;
 	} else {
