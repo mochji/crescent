093b4357ca66e879247a64beeb3ac9cdc84ab0ac
mochji <owo.mochji@gmail.com>
2026-01-29 02:02:35 -0500

    change gc parameters
    step: 4096 bytes until step
    multiplier: # of objects per kibibyte of memory allocated since last
    step

--- src/core/gc.h
+++ src/core/gc.h
@@ -48,19 +48,8 @@
  *   cycle.
  *
  * multiplier:
- *   Percent of the memory allocated since the last step (which may be more
- *   than the step parameter) that should be marked or swept in this step.
- *
- *   For example:
- *     - A value of 400 means it works at four times the rate of allocation.
- *     - A value of 200 means it works at twice the rate of allocation.
- *     - A value of 100 means it works at the rate of allocation.
- *     - A value of 0 means no work is ever done in a step.
- *
- *   Values less than or equal to 100 will most likely cause the collector to
- *   never finish a cycle, as it cannot keep up with the rate of allocation.
- *   Or (to use a metaphor) the outflow of memory is less than or equal to the
- *   inflow, never completely draining.
+ *   Amount of objects that should be processed (marked, traversed, or swept)
+ *   in a single step per kibibyte of memory allocated since the last step.
  */
 
 #define CRS_INDEX_GCPAUSE      0
@@ -69,8 +58,8 @@
 
 /* these are only defaults; they can be changed during runtime */
 #define CRS_GCP_PAUSE      100
-#define CRS_GCP_STEP       12
-#define CRS_GCP_MULTIPLIER 400
+#define CRS_GCP_STEP       4096
+#define CRS_GCP_MULTIPLIER 200
 
 #define gc_getparam(s, n)    ((s)->gc.params[CRS_INDEX_GC##n])
 #define gc_setparam(s, n, v) ((s)->gc.params[CRS_INDEX_GC##n] = (v))
