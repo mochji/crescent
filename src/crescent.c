c47ecde6d11857ea9aeb9ff043558ce549f84b00
mochji <owo.mochji@gmail.com>
2026-02-04 10:50:53 -0500

    add super-duper-cool-totally-not-a-waste-of-time testing thing

--- src/crescent.c
+++ src/crescent.c
@@ -10,21 +10,239 @@
 
 #include "api/api.h"
 
-int
-reallybad(crs_Thread* thread) {
-	crs_pushString(thread, "hahaha i'm an evil function ooOOoo");
-	crs_error(thread, 1);
+void
+stack(crs_Thread* thread) {
+	printf("pushing then popping ten\n");
+	for (int a = 0; a < 10; a++) {
+		crs_pushInteger(thread, a);
+	}
+	printf("top: %d\n", crs_getTop(thread));
 
-	return 0;
+	crs_pop(thread, 10);
+	printf("top: %d\n", crs_getTop(thread));
+
+	/* ==== */
+
+	printf("\npushing then removing ten\n");
+	for (int a = 0; a < 10; a++) {
+		crs_pushInteger(thread, a);
+	}
+	printf("top: %d\n", crs_getTop(thread));
+
+	for (int a = 0; a < 10; a++) {
+		crs_remove(thread, 1);
+	}
+	printf("top: %d\n", crs_getTop(thread));
+
+	/* ==== */
+
+	printf("\npushing ten then setting zero\n");
+	for (int a = 0; a < 10; a++) {
+		crs_pushInteger(thread, a);
+	}
+	printf("top: %d\n", crs_getTop(thread));
+
+	crs_setTop(thread, 0);
+	printf("top: %d\n", crs_getTop(thread));
+
+	/* ==== */
+
+	printf("\nsetting ten; should all be nil\n");
+	crs_setTop(thread, 10);
+
+	for (int a = 0; a < crs_getTop(thread); a++) {
+		printf("%s\n", crs_typeName(crs_type(thread, a)));
+	}
+	crs_setTop(thread, 0);
+
+	/* ==== */
+
+	printf("\nchecking top to 32\n");
+	if (!crs_checkTop(thread, 32)) {
+		printf("failed!\n");
+	}
+
+	for (int a = 0; a < 32; a++) {
+		crs_pushNil(thread);
+	}
+	printf("%d\n", crs_getTop(thread));
+
+	crs_setTop(thread, 0);
+}
+
+void
+types(crs_Thread* thread) {
+	printf("expected types and values:\n");
+	printf("- nil\n");
+	printf("- boolean, 1\n");
+	printf("- number, -5\n");
+	printf("- number, 20.913\n");
+	printf("- string, startrix is best\n");
+
+	crs_pushNil(thread);
+	crs_pushBoolean(thread, 1);
+	crs_pushInteger(thread, -5);
+	crs_pushFloat(thread, 20.913);
+	crs_pushString(thread, "startrix is best");
+
+	printf("actual types and values:\n");
+
+	for (int a = 1; a <= crs_getTop(thread); a++) {
+		int type = crs_type(thread, a);
+
+		printf("- %s", crs_typeName(type));
+
+		switch (type) {
+			case CRS_TYPE_BOOLEAN:
+				printf(", %d", crs_toBoolean(thread, a)); break;
+			case CRS_TYPE_INTEGER:
+				printf(", " CRS_INTEGER_FMT, crs_toInteger(thread, a)); break;
+			case CRS_TYPE_FLOAT:
+				printf(", " CRS_FLOAT_FMT, crs_toFloat(thread, a)); break;
+			case CRS_TYPE_STRING:
+				printf(", %s", crs_toString(thread, a)); break;
+		}
+
+		printf("\n");
+	}
+
+	crs_setTop(thread, 0);
 }
 
 int
 test(crs_Thread* thread) {
-	crs_pushString(thread, "hello, world!");
+	printf("passed args: %d\n", crs_getTop(thread));
+	for (int a = 1; a <= crs_getTop(thread); a++) {
+		printf("- %s\n", crs_typeName(crs_type(thread, a)));
+	}
 
-	return 1;
+	crs_pushInteger(thread, 413);
+	crs_pushInteger(thread, 612);
+
+	printf("expected returned:\n");
+	printf("- function\n- number\n- number\n");
+
+	return 3;
 }
 
+void
+calling(crs_Thread* thread) {
+	crs_pushString(thread, "hello, world");
+	crs_pushCFunction(thread, &test);
+
+	printf("expected args: %d\n", crs_getTop(thread));
+	for (int a = 1; a <= crs_getTop(thread); a++) {
+		printf("- %s\n", crs_typeName(crs_type(thread, a)));
+	}
+
+	crs_call(thread, -1, 2);
+
+	printf("returned: %d\n", crs_getTop(thread));
+	for (int a = 1; a <= crs_getTop(thread); a++) {
+		printf("- %s\n", crs_typeName(crs_type(thread, a)));
+	}
+
+	crs_setTop(thread, 0);
+}
+
+int
+mischievous(crs_Thread* thread) {
+	crs_pushString(thread, "error message");
+	crs_error(thread, 2);
+
+	return 0;
+}
+
+void
+handling(crs_Thread* thread) {
+	int status;
+
+	crs_pushCFunction(thread, &mischievous);
+	crs_pCall(thread, 1, 1, &status);
+
+	printf("top: %d\n", crs_getTop(thread));
+	for (int a = 1; a <= crs_getTop(thread); a++) {
+		printf("- %s\n", crs_typeName(crs_type(thread, a)));
+	}
+
+	if (status != CRS_STATUS_OK) {
+		printf("errored! %s\n", crs_toString(thread, 1));
+	} else {
+		printf("ok?\n");
+	}
+
+	crs_setTop(thread, 0);
+}
+
+void
+panicking(crs_Thread* thread) {
+	crs_pushCFunction(thread, &mischievous);
+	crs_call(thread, 1, 0);
+}
+
+void
+choose(crs_Thread* thread, char option) {
+	printf("========\n");
+
+	switch (option) {
+		case 's':
+			stack(thread);
+
+			break;
+		case 't':
+			types(thread);
+
+			break;
+		case 'c':
+			calling(thread);
+
+			break;
+		case 'h':
+			handling(thread);
+
+			break;
+		case 'p':
+			panicking(thread);
+
+			break;
+		case 'q':
+			printf("bye :(\n");
+
+			break;
+	}
+
+	printf("========\n");
+}
+
+int
+main(void) {
+	crs_Thread* thread = crs_open();
+
+	printf("hi you wanna test stuff\n");
+
+	char option;
+
+	do {
+		printf("s: stack\n");
+		printf("t: types\n");
+		printf("c: calling\n");
+		printf("h: handling\n");
+		printf("p: panicking\n");
+		printf("q: quit\n");
+		printf("> ");
+
+		do {
+			option = getchar();
+		} while (option == '\n');
+
+		choose(thread, option);
+	} while (option != 'q');
+
+	crs_close(thread);
+
+	return 0;
+}
+/*
 int
 main(void) {
 	crs_Thread* thread = crs_open();
@@ -80,4 +298,4 @@ main(void) {
 	crs_close(thread);
 
 	return 0;
-}
+}*/
