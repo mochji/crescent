b8002fedb2e9cc97103ff4db801f3faadc42a59f
mochji <owo.mochji@gmail.com>
2026-04-03 00:12:40 -0400

    add crs_Buffer

---
+++ src/core/buffer.c
@@ -0,0 +1,88 @@
+/*
+ * https://github.com/mochji/crescent
+ * core/buffer.c
+ *
+ * Copyright (C) 2026 mochji
+ * MIT License
+ */
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "conf.h"
+#include "limit.h"
+
+#include "core/memory.h"
+#include "core/call.h"
+
+#include "core/buffer.h"
+
+static void
+growBuffer(crs_Buffer* buffer, size_t needed) {
+	char*  vector = buffer->buffer;
+	size_t size   = buffer->size;
+
+	while (size < needed) {
+		size = size > (SIZE_MAX >> 1)
+			? SIZE_MAX
+			: size << 1;
+	}
+
+	if (vector == buffer->initial) {
+		vector = mem_vnew(buffer->thread, size, char);
+
+		if (vector != NULL) {
+			memcpy(vector, buffer->initial, buffer->length);
+		}
+	} else {
+		vector = mem_vresize(buffer->thread, vector, size, buffer->size);
+	}
+
+	if (vector == NULL) {
+		crsB_free(buffer);
+		crsM_error(buffer->thread);
+	}
+
+	buffer->size   = size;
+	buffer->buffer = vector;
+}
+
+static void
+checkBuffer(crs_Buffer* buffer, size_t length) {
+	if (length > SIZE_MAX - buffer->length) {
+		crsB_free(buffer);
+		crsC_error(buffer->thread, "buffer overflow");
+	}
+
+	if (buffer->length + length > buffer->size) {
+		growBuffer(buffer, buffer->length + length);
+	}
+}
+
+void
+crsB_init(crs_Thread* thread, crs_Buffer* buffer) {
+	buffer->thread = thread;
+	buffer->size   = CRS_BUF_INITIAL;
+	buffer->length = 0;
+	buffer->buffer = buffer->initial;
+}
+
+void
+crsB_free(crs_Buffer* buffer) {
+	if (buffer->buffer != buffer->initial) {
+		mem_vfree(buffer->thread, buffer->buffer, buffer->size);
+	}
+}
+
+void
+crsB_addString(crs_Buffer* buffer, char* str, size_t length) {
+	checkBuffer(buffer, length);
+
+	char* vector    = buffer->buffer + buffer->length;
+	buffer->length += length;
+
+	while (length--) {
+		*vector++ = *str++;
+	}
+}
---
+++ src/core/buffer.h
@@ -0,0 +1,39 @@
+/*
+ * https://github.com/mochji/crescent
+ * core/buffer.h
+ *
+ * Copyright (C) 2026 mochji
+ * MIT License
+ */
+
+#ifndef CRS_CORE_BUFFER_H
+#define CRS_CORE_BUFFER_H
+
+#include <stddef.h>
+
+#include "conf.h"
+#include "limit.h"
+
+#define CRS_BUF_INITIAL 256
+
+struct
+crs_Buffer {
+	crs_Thread* thread;
+	size_t      size;
+	size_t      length;
+	char*       buffer;
+	char        initial[CRS_BUF_INITIAL];
+};
+
+typedef struct crs_Buffer crs_Buffer;
+
+void
+crsB_init(crs_Thread* thread, crs_Buffer* buffer);
+
+void
+crsB_free(crs_Buffer* buffer);
+
+void
+crsB_addString(crs_Buffer* buffer, char* str, size_t length);
+
+#endif
